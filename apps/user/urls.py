5d317c2fc4a8e9b6ba092db7e81ea5b40e10eb86
ignacio <ignaciomenchaca666@gmail.com>
2018-09-11 00:01:56 -0500

    edit terminado...creo

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from apps.user.views import index, create, delete, table, search
+from apps.user.views import *
 
 app_name = 'user'
 urlpatterns = [
     path('', index, name='index'),
     path('create/', create, name='create'),
     path('delete/<slug:code>/', delete, name='delete'),
+    path('edit/<slug:code>/', edit, name='edit'),
     path('table/', table, name='table'),
     path('search/<slug:find>/', search, name='search'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from apps.user.models import User
 from apps.user.form import UserForm
 from passlib.hash import pbkdf2_sha256  # pip install passlib
@@ -29,19 +29,27 @@ def create(request):
     return render(request, 'user/create.html', {'form': UserForm})
 
 
-def edit(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            sweetify.success(request, 'Cambios aplicados correctamente!', toast=True, position='top', timer=2000)
-            return redirect('user:index')
-    return render(request, 'user/edit.html', {'form': UserForm})
-
-
 def show(request, code):
     user = User.objects.get(pk=code)
     return render(request, 'user/show.html', {'object_list': user})
+
+
+def edit(request, code):
+    if request.method == 'POST':
+        User.objects.filter(pk=code).update(
+            password=pbkdf2_sha256.encrypt(request.POST['password'], rounds=12000, salt_size=32),
+            nombre=request.POST['nombre'],
+            apaterno=request.POST['apaterno'],
+            amaterno=request.POST['amaterno'],
+            email=request.POST['email'],
+            address=request.POST['address'],
+            phone=request.POST['phone'],
+            role_id=request.POST['role'],
+        )
+        sweetify.success(request, 'Editado correctamente!', toast=True, position='top', timer=2000)
+        return redirect('user:index')
+    user = User.objects.get(pk=code)
+    return render(request, 'user/edit.html', {'form': UserForm(instance=user)})
 
 
 def delete(request, code):
