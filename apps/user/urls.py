e68827da8d8769e4aa6b9adfdd4fe7b979803359
ignacio <ignaciomenchaca666@gmail.com>
2018-09-27 21:13:32 -0500

    cambios menores

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required as auth
 from apps.user.views import *
 
-
 app_name = 'user'
 
 urlpatterns = [
@@ -11,6 +10,6 @@ urlpatterns = [
     path('edit/<slug:code>/', auth(edit), name='edit'),
     path('show/<slug:code>/', auth(show), name='show'),
     path('delete/<slug:code>/', auth(delete), name='delete'),
-    path('table/', auth(table), name='table'),
     path('search/<slug:find>/', auth(search), name='search'),
+    path('table/', auth(table), name='table'),
 ]
