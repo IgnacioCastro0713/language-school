0bbc9e31f2efe7387bc53e1564056e4469d3348a
ignacio <ignaciomenchaca666@gmail.com>
2018-09-29 00:27:32 -0500

    paginado funcional

--- apps/classroom/views.py
+++ apps/classroom/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, redirect
 from apps.classroom.models import Classroom
 from apps.classroom.form import ClassroomForm
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from sweetify import *
 
 
 def index(request):
-    classrooms = Classroom.objects.all()
+    classrooms_list = Classroom.objects.all()
+    paginator = Paginator(classrooms_list, 2)
+    page = request.GET.get('page')
+    # classrooms = paginator.get_page(page)
+
+    try:
+        classrooms = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        classrooms = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        classrooms = paginator.page(paginator.num_pages)
+
     return render(request, 'classroom/index.html', {
         'object_list': classrooms,
         'title': 'Aulas',
@@ -45,8 +58,23 @@ def delete(request, id_class):
 
 
 def table(request):
-    classrooms = Classroom.objects.all()
-    return render(request, 'classroom/table.html', {'object_list': classrooms})
+    classrooms_list = Classroom.objects.all()
+    paginator = Paginator(classrooms_list, 2)
+    page = request.GET.get('page')
+    # classrooms = paginator.get_page(page)
+
+    try:
+        classrooms = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        classrooms = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        classrooms = paginator.page(paginator.num_pages)
+
+    return render(request, 'classroom/table.html', {
+        'object_list': classrooms,
+    })
 
 
 def search(request, find):
