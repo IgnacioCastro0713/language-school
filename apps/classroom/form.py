8333cbfbc65733f3ae406a1f128169a36b896960
ignacio <ignaciomenchaca666@gmail.com>
2018-10-04 15:57:41 -0500

    Valiadción de classroom

--- apps/classroom/form.py
+++ apps/classroom/form.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from apps.classroom.models import Classroom
 
 
 class ClassroomForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ClassroomForm, self).__init__(*args, **kwargs)
+        self.fields['name'].required = False
+        self.fields['building'].required = False
 
     class Meta:
 
@@ -22,6 +29,17 @@ class ClassroomForm(forms.ModelForm):
                 attrs={
                     'class': 'form-control',
                     'placeholder': 'nombre edificio',
-                }
-            ),
+                }),
         }
+
+    def clean_name(self):
+        data = self.cleaned_data
+        if not data['name']:
+            raise ValidationError('El campo nombre esta vació')
+        return data['name']
+
+    def clean_building(self):
+        data = self.cleaned_data
+        if not data['building']:
+            raise ValidationError('El campo edificio esta vació')
+        return data['building']
