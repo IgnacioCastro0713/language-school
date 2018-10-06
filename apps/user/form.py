35bd3e60e90c5dad9f5a2960214edb4c2a9af543
ignacio <ignaciomenchaca666@gmail.com>
2018-10-06 13:21:22 -0500

    validacion en el form de usuario

--- apps/user/form.py
+++ apps/user/form.py
@@ -85,17 +85,53 @@ class UserForm(forms.ModelForm):
     def clean_code(self):
         data = self.cleaned_data
         if not data['code']:
-            raise ValidationError('El campo código esta vacio')
+            raise ValidationError('El campo código esta vació')
         return data['code']
 
     def clean_password(self):
         data = self.cleaned_data
         if not data['password']:
-            raise ValidationError('El campo contraseña esta vacio')
+            raise ValidationError('El campo contraseña esta vació')
         return data['password']
+
+    def clean_first_name(self):
+        data = self.cleaned_data
+        if not data['first_name']:
+            raise ValidationError('El campo nombre esta vació')
+        return data['first_name']
+
+    def clean_last_name(self):
+        data = self.cleaned_data
+        if not data['last_name']:
+            raise ValidationError('El campo apellido paterno esta vació')
+        return data['last_name']
+
+    def clean_second_last_name(self):
+        data = self.cleaned_data
+        if not data['second_last_name']:
+            raise ValidationError('El campo apellido materno esta vació')
+        return data['second_last_name']
 
     def clean_email(self):
         data = self.cleaned_data
         if not data['email']:
             raise ValidationError('El campo correo electronico esta vacio')
         return data['email']
+
+    def clean_address(self):
+        data = self.cleaned_data
+        if not data['address']:
+            raise ValidationError('El campo dirección esta vació')
+        return data['address']
+
+    def clean_phone(self):
+        data = self.cleaned_data
+        if not data['phone']:
+            raise ValidationError('El campo apellido telefono esta vació')
+        return data['phone']
+
+    def clean_role(self):
+        data = self.cleaned_data
+        if not data['role']:
+            raise ValidationError('Debe seleccionar un rol para este usuario')
+        return data['role']
