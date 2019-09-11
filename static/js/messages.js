de82ffc7c62f827b060c4a37e93e068da46ca6fd
IgnacioCastro <ignaciomenchaca666@gmail.com>
2019-09-11 16:34:01 -0500

    2 tabs in javascript and html5

--- static/js/messages.js
+++ static/js/messages.js
@@ -1,78 +1,78 @@
 import swal from 'sweetalert2';
 
 const swalMaterial = swal.mixin({
-    confirmButtonClass: 'btn btn-success',
-    cancelButtonClass: 'btn btn-danger',
-    buttonsStyling: false,
+  confirmButtonClass: 'btn btn-success',
+  cancelButtonClass: 'btn btn-danger',
+  buttonsStyling: false,
 });
 
 const toast = swal.mixin({
-    toast: true,
-    position: 'top',
-    showConfirmButton: false,
-    timer: 3000,
-    showCloseButton: false
+  toast: true,
+  position: 'top',
+  showConfirmButton: false,
+  timer: 3000,
+  showCloseButton: false
 });
 
 const confirmDelete = (e, name, url) => {
-    e.preventDefault();
-    deleteElement(name, url, function () {
-        toast({
-            title: 'Eliminado correctamente.',
-            type: 'success'
-        });
-        loadTable(e, false);
-    }, function () {
-        toast({
-            title: 'No se ha podido eliminar el elemento.',
-            type: 'error'
-        });
-        loadTable(e, false);
+  e.preventDefault();
+  deleteElement(name, url, function () {
+    toast({
+      title: 'Eliminado correctamente.',
+      type: 'success'
     });
+    loadTable(e, false);
+  }, function () {
+    toast({
+      title: 'No se ha podido eliminar el elemento.',
+      type: 'error'
+    });
+    loadTable(e, false);
+  });
 };
 
 const deleteInShow = (e, name, url, index) => {
-    e.preventDefault();
-    deleteElement(name, url, function () {
-        toast({
-            title: 'Eliminado correctamente.',
-            type: 'success'
-        });
-        window.location.replace(index);
-    }, function () {
-        toast({
-            title: 'No se ha podido eliminar el elemento.',
-            type: 'error'
-        });
+  e.preventDefault();
+  deleteElement(name, url, function () {
+    toast({
+      title: 'Eliminado correctamente.',
+      type: 'success'
     });
+    window.location.replace(index);
+  }, function () {
+    toast({
+      title: 'No se ha podido eliminar el elemento.',
+      type: 'error'
+    });
+  });
 };
 
 const deleteElement = (name, url, done, fail) => {
-    swalMaterial({
-        title: '¿Desea eliminar el elemento?',
-        text: "Está apunto de eliminar el registro de "+name+".",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonText: '<i class="now-ui-icons ui-1_check"></i>  Eliminar',
-        cancelButtonText: '<i class="now-ui-icons ui-1_simple-remove"></i>  Cancelar',
-    }).then((result) => {
-        if (result.value) {
-            $.ajax({
-                data: {
-                    '_method' : 'DELETE',
-                    csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val() || '',
-                },
-                type: 'post',
-                url: url,
-            }).done(done).fail(fail)
-        }
-    })
+  swalMaterial({
+    title: '¿Desea eliminar el elemento?',
+    text: "Está apunto de eliminar el registro de " + name + ".",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonText: '<i class="now-ui-icons ui-1_check"></i>  Eliminar',
+    cancelButtonText: '<i class="now-ui-icons ui-1_simple-remove"></i>  Cancelar',
+  }).then((result) => {
+    if (result.value) {
+      $.ajax({
+        data: {
+          '_method': 'DELETE',
+          csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val() || '',
+        },
+        type: 'post',
+        url: url,
+      }).done(done).fail(fail)
+    }
+  })
 };
 
 export default {
-    toast:         window.toast = toast,
-    swalMaterial:  window.swalMaterial = swalMaterial,
-    confirmDelete: window.confirmDelete = confirmDelete,
-    deleteInShow:  window.deleteInShow = deleteInShow,
-    deleteElement: window.deleteElement = deleteElement
+  toast: window.toast = toast,
+  swalMaterial: window.swalMaterial = swalMaterial,
+  confirmDelete: window.confirmDelete = confirmDelete,
+  deleteInShow: window.deleteInShow = deleteInShow,
+  deleteElement: window.deleteElement = deleteElement
 };
--- static/js/pagination.js
+++ static/js/pagination.js
@@ -1,41 +1,41 @@
 var opcUrl = table;
 
 const changePage = (obj, e) => {
-    e.preventDefault();
-    let page = $(obj).attr('href').split('?page=')[1];
-    let param = $('#search').val();
-    if (param !== "")
-        opcUrl = search+'/'+param+'/';
-    else
-        opcUrl = table;
-    $.ajax({
-        url : opcUrl+'?page='+page
-    }).done(function (response) {
-        $('#content').html(response);
-        location.hash = page;
-        $('html, body').stop().animate({
-            scrollTop: $("#top").offset().top
-        }, 250)
-    })
+  e.preventDefault();
+  let page = $(obj).attr('href').split('?page=')[1];
+  let param = $('#search').val();
+  if (param !== "")
+    opcUrl = search + '/' + param + '/';
+  else
+    opcUrl = table;
+  $.ajax({
+    url: opcUrl + '?page=' + page
+  }).done(function (response) {
+    $('#content').html(response);
+    location.hash = page;
+    $('html, body').stop().animate({
+      scrollTop: $("#top").offset().top
+    }, 250)
+  })
 };
 
 var currentUrl = table;
 
 const loadTable = (e, doSearch) => {
-    e.preventDefault();
-    let param = $('#search').val();
-    if (doSearch && param !== "")
-        currentUrl = search+'/'+param+'/';
-    else
-        currentUrl = table;
-    $.ajax({
-        url : currentUrl,
-        method : 'get',
-        csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val() || '',
-    }).done(function (response) {
-        currentUrl = search;
-        $('#content').html(response);
-        if (doSearch)
-            location.hash = $('#search').val();
-    })
+  e.preventDefault();
+  let param = $('#search').val();
+  if (doSearch && param !== "")
+    currentUrl = search + '/' + param + '/';
+  else
+    currentUrl = table;
+  $.ajax({
+    url: currentUrl,
+    method: 'get',
+    csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val() || '',
+  }).done(function (response) {
+    currentUrl = search;
+    $('#content').html(response);
+    if (doSearch)
+      location.hash = $('#search').val();
+  })
 };
